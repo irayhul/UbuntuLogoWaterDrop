ff2789a36fa6096d9f8cdb388efc2d7574457797
irayhul <rahulsharmaa295@gmail.com>
2023-09-22 10:49:45 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from "./Components/Home/Home.jsx"
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+    <Home />
+ 
+  </div>
+);
+}
+
+export default App
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.css"
+const Home = () => {
+  return (
+<div className="drop">
+  <i className="fab fa-ubuntu" />
+</div>
+
+  )
+}
+
+export default Home
